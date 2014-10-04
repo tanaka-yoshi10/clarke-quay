8fa0d598b8dce6c087f780673182c700f60de304
Yoshito Tanaka <tanaka.yoshi-10@gmail.com>
2014-10-04 14:39:05 +0900

    add led temp scripts

---
+++ led.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+GPIO = 24
+
+exp = open("/sys/class/gpio/export", "w")
+exp.write(GPIO)
+exp.close
+ 
+dir = open("/sys/class/gpio/gpio#{GPIO}/direction", "w")
+dir.write("out")
+dir.close
+ 
+out = 1
+20.times do
+  val = open("/sys/class/gpio/gpio#{GPIO}/value", "w")
+  val.write(out)
+  val.close
+  out = out == 1 ? 0 : 1
+  sleep 0.5
+end
+ 
+uexp = open("/sys/class/gpio/unexport", "w")
+uexp.write(GPIO)
+uexp.close
